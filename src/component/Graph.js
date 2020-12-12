a973c1407cb5effdb20af9fd7068c1856b529b92
Ramazan Çetin <ramazancetinn@yandex.com>
2020-12-12 18:56:45 +0300

    Add icons

---
+++ src/component/Graph.js
@@ -0,0 +1,43 @@
+import * as React from "react";
+
+function SvgGraph(props) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      viewBox="0 0 18.336 18.332"
+      {...props}
+    >
+      <g data-name="Group 72">
+        <g data-name="Group 71">
+          <path
+            data-name="Path 181"
+            d="M4.584 7.2H.656A.658.658 0 000 7.856v9.82a.656.656 0 00.656.656h3.928a.656.656 0 00.656-.656v-9.82a.656.656 0 00-.656-.656z"
+            fill="currentColor"
+          />
+        </g>
+      </g>
+      <g data-name="Group 74">
+        <g data-name="Group 73">
+          <path
+            data-name="Path 182"
+            d="M11.132 0H7.204a.655.655 0 00-.656.65v17.026a.656.656 0 00.656.656h3.928a.656.656 0 00.656-.656V.656A.656.656 0 0011.132 0z"
+            fill="currentColor"
+          />
+        </g>
+      </g>
+      <g data-name="Group 76">
+        <g data-name="Group 75">
+          <path
+            data-name="Path 183"
+            d="M17.68 5.236h-3.928a.653.653 0 00-.656.656v11.784a.656.656 0 00.656.656h3.928a.656.656 0 00.656-.656V5.892a.656.656 0 00-.656-.656z"
+            fill="currentColor"
+          />
+        </g>
+      </g>
+    </svg>
+  );
+}
+
+export default SvgGraph;
