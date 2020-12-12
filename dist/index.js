ba4b71646ae6cbfaf70f773e75800ddb13504996
Ramazan Çetin <ramazancetinn@yandex.com>
2020-12-12 18:54:03 +0300

    Update package

--- dist/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Account } from './Account';
-export { default as Graph } from './Graph';
-export { default as Insurance } from './Insurance';
-export { default as Mail } from './Mail';
