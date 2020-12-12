5130d8a0cbd80f52354d6327b0a214309155eb34
Ramazan Çetin <ramazancetinn@yandex.com>
2020-12-12 19:11:19 +0300

    patch

--- src/component/index.js
+++ src/component/index.js
@@ -1,4 +1,11 @@
-export { default as Account } from './Account'
-export { default as Graph } from './Graph'
-export { default as Insurance } from './Insurance'
-export { default as Mail } from './Mail'
+import Account from "./Account";
+import Graph from "./Graph";
+import Insurance from "./Insurance";
+import Mail from "./Mail";
+
+module.exports = {
+  Account,
+  Graph,
+  Insurance,
+  Mail,
+};
