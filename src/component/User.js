f690e72a77beece662d047fdde80ca222c44d284
Ramazan Çetin <ramazancetinn@yandex.com>
2020-12-13 13:11:06 +0300

    Add user icon

---
+++ src/component/User.js
@@ -0,0 +1,51 @@
+import * as React from "react";
+
+function SvgUser(props) {
+  return (
+    <svg
+      id="user_svg__user_1_"
+      data-name="user"
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      viewBox="0 0 52.942 59.145"
+      {...props}
+    >
+      <defs>
+        <style>{".user_svg__cls-1{fill:#948a9a}"}</style>
+      </defs>
+      <g
+        id="user_svg__Group_80"
+        data-name="Group 80"
+        transform="translate(11.119)"
+      >
+        <g id="user_svg__Group_79" data-name="Group 79">
+          <path
+            id="user_svg__Path_190"
+            data-name="Path 190"
+            className="user_svg__cls-1"
+            d="M126.147 0A15.356 15.356 0 10141.5 15.356 15.4 15.4 0 00126.147 0z"
+            transform="translate(-110.791)"
+          />
+        </g>
+      </g>
+      <g
+        id="user_svg__Group_82"
+        data-name="Group 82"
+        transform="translate(0 31.811)"
+      >
+        <g id="user_svg__Group_81" data-name="Group 81">
+          <path
+            id="user_svg__Path_191"
+            data-name="Path 191"
+            className="user_svg__cls-1"
+            d="M76.994 259.014a14.315 14.315 0 00-1.542-2.816 19.088 19.088 0 00-13.21-8.315 2.826 2.826 0 00-1.945.469 16.369 16.369 0 01-19.313 0 2.516 2.516 0 00-1.945-.469 18.949 18.949 0 00-13.21 8.315 16.505 16.505 0 00-1.542 2.816 1.426 1.426 0 00.067 1.274 26.76 26.76 0 001.811 2.682 25.489 25.489 0 003.085 3.487 40.212 40.212 0 003.085 2.682 30.581 30.581 0 0036.48 0 29.5 29.5 0 003.085-2.682 30.981 30.981 0 003.085-3.487 23.518 23.518 0 001.811-2.682 1.144 1.144 0 00.198-1.274z"
+            transform="translate(-24.165 -247.841)"
+          />
+        </g>
+      </g>
+    </svg>
+  );
+}
+
+export default SvgUser;
